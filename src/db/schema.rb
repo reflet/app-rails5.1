1b86905b7933eeb8a6f4d382ab55ad34986c7880
reflet <reflet.jp@gmail.com>
2018-11-13 15:32:56 +0900

    Diaryモデルを追加 ( $ rails g model Diary title:string body:text )

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20181113062352) do
+
+  create_table "diaries", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
